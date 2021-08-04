379ff69cae3b7377df88a40d69621532cacb0d7e
Black <vothanhlam1793@gmail.com>
2021-08-04 23:13:24 +0700

    lung tung ben

--- client/main.js
+++ client/main.js
@@ -4,11 +4,11 @@ import '../imports/ui/plugins'
 
 import App from '../imports/ui/App.vue'
 
-import router from './routes'
+// import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 
 Meteor.startup(() => {
   new Vue({
-    router,
-    render: (h)=>h(App)
-  }).$mount("#app");
+    el: "#app",
+    ...App
+  })
 })
--- server/main.js
+++ server/main.js
@@ -5,3 +5,29 @@ import '../imports/api/publications'
 import '../imports/api/methods/methodContact'
 
 import {ContactsCollection} from '../imports/api/collections/Contact'
+
+// import { Meteor } from 'meteor/meteor';
+// import Vue from 'vue';
+// import { onPageLoad } from 'meteor/server-render';
+// import { createRenderer } from 'vue-server-renderer';
+// const renderer = createRenderer();
+
+// onPageLoad(sink => {
+//     console.log('onPageLoad');
+//     const url = sink.request.url.path;
+//     const app = new Vue({
+//         data: {
+//             url
+//         },
+//         template: `<div>The visited URL is: {{ url }}</div>`
+//     });
+
+//     renderer.renderToString(app, (err, html) => {
+//         if (err) {
+//             res.status(500).end('Internal Server Error');
+//             return
+//         }
+//         console.log('html', html);
+//         sink.renderIntoElementById('app', html);
+//     })
+// })
